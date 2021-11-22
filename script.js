98746416075cc9e54967cde4f5739154bdb6689f
Guto <augustopinheiro19999999@gmail.com>
2021-11-22 19:53:18 -0300

    Todos Requisitos, exceto uma parte do requisito 10

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-const divPai = document.getElementById('pixel-board')
 
-function addClassToAll() {
-    for(let i = 0; i < divPai.children.length; i += 1) {
-        divPai.children[i].className = 'pixel'
+
+
+// aqui eu pego o valor do input pelo acionamento do botão VQV para usar como 'n'
+const btn2 = document.getElementById("generate-board")
+const inputSize = document.getElementById("board-size")
+
+btn2.addEventListener('click', boardSize)
+
+let n = 5
+
+let blankBlocks = document.getElementsByClassName('pixel')
+
+function boardSize() {
+    if(inputSize.value == ''){
+        alert('Board inválido!')
+    } else if(inputSize.value < 5) {
+        n = 5
+        createBoard(n)
+    } else if(inputSize.value > 50) {
+        n = 50
+        createBoard(n)
+    } else {
+        n = inputSize.value
+        // console.log(inputSize.value)
+        createBoard(n)
     }
-}
+    
+    for(let i3 = 0; i3 < blankBlocks.length; i3 += 1) {
+        blankBlocks[i3].addEventListener('click', useColor) // tinha fazer isso for de novo
+    }
 
-addClassToAll()
+    
+}   
+
+
+const local = document.getElementById("pixel-board")
+
+
+function createBoard(n) {
+    local.innerHTML = ''
+    for(let u = 0; u < (n*n); u += 1 ) {
+      let block = document.createElement("div")
+      block.className = 'pixel'
+      block.style.backgroundColor = 'white'
+      local.appendChild(block)
+    }
+    local.style.width = ((40 + 2)* n) + 'px'
+    local.style.height = ((40 + 2)* n) + 'px'
+}
+createBoard(n)
+
+
+
 
 const colorBlocks = document.getElementsByClassName('color')
 
@@ -27,7 +72,6 @@ function getColor(evt) {
     currentColor = currentBlock.backgroundColor
 }
 
-const blankBlocks = document.getElementsByClassName('pixel')
 
 for(let i3 = 0; i3 < blankBlocks.length; i3 += 1) {
     blankBlocks[i3].addEventListener('click', useColor) 
@@ -47,3 +91,33 @@ function clearBoard() {
         blankBlocks[i3].style.backgroundColor = 'white'
     }
 }
+
+blankBlocks[0].addEventListener('click', test)
+
+function test() {
+    console.log('foi')
+}
+
+let numA1 = Math.round(Math.random()*255)
+let numA2 = Math.round(Math.random()*255)
+let numA3 = Math.round(Math.random()*255)
+
+let rgbName1 = 'rgb(' + numA1 + ',' + numA2 + ',' + numA3 + ')'
+
+let numB1 = Math.round(Math.random()*255)
+let numB2 = Math.round(Math.random()*255)
+let numB3 = Math.round(Math.random()*255)
+
+let rgbName2 = 'rgb(' + numB1 + ',' + numB2 + ',' + numB3 + ')'
+
+let numC1 = Math.round(Math.random()*255)
+let numC2 = Math.round(Math.random()*255)
+let numC3 = Math.round(Math.random()*255)
+
+let rgbName3 = 'rgb(' + numC1 + ',' + numC2 + ',' + numC3 + ')'
+
+
+
+colorBlocks[1].style.backgroundColor = rgbName1
+colorBlocks[2].style.backgroundColor = rgbName2
+colorBlocks[3].style.backgroundColor = rgbName3
