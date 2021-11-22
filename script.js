cf3595ba76e76a9dca9fd8a2e637352bc208c986
Guto <augustopinheiro19999999@gmail.com>
2021-11-22 15:27:21 -0300

    Terminei, consegui pegar o estilo dos blocos

--- script.js
+++ script.js
@@ -16,20 +16,31 @@ for(let i2 = 0; i2 < colorBlocks.length; i2 += 1) {
     console.log('ha')
 }
 
+let currentBlock;
 let currentColor;
 
 function getColor(evt) {
-    // currentColor = evt.target.style.backgroundColor
-    // evt.target.style.backgroundColor = 'orange'
-    console.log(evt.target.style.backgroundColor)
+    currentBlock = window.getComputedStyle(evt.target)  // tinha aprendido (e referênciado) este método no exercicio do dia5-3.
+
+    // console.log(evt.target)
+    currentColor = currentBlock.backgroundColor
+    // console.log(currentColor)
 }
 
-// const blankBlocks = document.getElementsByClassName('block')
+const blankBlocks = document.getElementsByClassName('block')
 
-// for(let i3 = 0; i3 < blankBlocks.length; i3 += 1) {
-//     blankBlocks[i3].addEventListener('click', useColor) 
-// }
+for(let i3 = 0; i3 < blankBlocks.length; i3 += 1) {
+    blankBlocks[i3].addEventListener('click', useColor) 
+}
 
-// function useColor(evt) {
-//     evt.target.backgroundColor = currentColor
-// }
+function useColor(evt) {
+    evt.target.style.backgroundColor = currentColor
+    // console.log(evt.target)
+}
+
+
+// let color1 = colorBlocks[0]
+
+// let blockCS = window.getComputedStyle(color1)
+
+// console.log(blockCS.backgroundColor)
